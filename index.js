e133e8176692ba5c8e7839da5e9ca9ed5160f1fe
jeremiasok <jereok91@gmail.com>
2020-05-01 03:33:29 -0500

    Corrct001

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const path=require('path');
+const path = require('path');
 const app = require('express')();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
-const routerOs= require('./src/router')
+const routerOs = require('./src/router')
 app.set('port', process.env.PORT || 3000);
 
 publicDir = `${__dirname}/src/public`
@@ -12,24 +12,25 @@ publicDir = `${__dirname}/src/public`
 // app.get('/', routerOs);
 
 
-	app.get('/', (req, res) => {
-		res.sendFile(`${publicDir}/client.html`)
-	})
-	app.get('/streaming', (req, res) => {
-		res.sendFile(`${publicDir}/server.html`)		
-	})
+app.get('/', (req, res) => {
+	res.sendFile(`${publicDir}/client.html`)
+})
+app.get('/streaming', (req, res) => {
+	res.sendFile(`${publicDir}/server.html`)
+})
 
 
 // socket
 io.on('connection', (socket) => {
-    socket.on('streaming', (image) => {
-		io.emit('play stream', image)
+	socket.on('streaming', (image) => {
+		socket.broadcast.emit('play stream', image)
 		//console.log(image)
-  
-})});
+
+	})
+});
 
 
 //sever escuchando 
-server.listen(app.get('port'), () =>{
-    console.log(`server on port localhost:${app.get('port')}`);
+server.listen(app.get('port'), () => {
+	console.log(`server on port localhost:${app.get('port')}`);
 });
